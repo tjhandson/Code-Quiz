01cd18aa070c6e3712b44eca56adaa9fb2326b24
tjhandson <73173000+tjhandson@users.noreply.github.com>
2023-01-24 21:11:26 +0000

    adding timer function to js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,6 +7,17 @@ const questionTitle = document.querySelector("#question-title");
 const questionChoices = document.querySelector("#choices");
 const finalScore = document.querySelector("#final-score");
 
+// Stating Variables Key
+
+let timer;
+let timerCount;
+let score;
+let correct = 0;
+
+
+// Timer Starting Text
+timeElement.textContent = "Ready!";
+
 // Sound Components
 let soundOn = true;
 const correctSound = new Audio('./assets/sfx/correct.wav');
@@ -21,3 +32,40 @@ function displayElement(element) {
     element.className =
         element.className.replace("hide", "start");
 }
+
+//Timer Function and shows question page 
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function () {
+        if (timerCount <= 0) {
+            // Clears interval and Ends Game if Time Runs out
+            clearInterval(timer);
+            displayElement(endScreen);
+            hideElement(questionScreen);
+        }
+        else {
+            timerCount--;
+            timeElement.textContent = timerCount;
+            finalScore.textContent = correct;
+            if (timerCount <= 10) {
+                timeElement.style.color = "#FF0000";
+            }
+        }
+    }, 1000);
+}
+
+// Start Game Sequence
+function startGame() {
+    startButton.disabled = true;
+    timerCount = 60;
+    startTimer();
+
+
+}
+
+
+
+// Event Lister to start Game
+startButton.addEventListener("click", startGame);
+
+
